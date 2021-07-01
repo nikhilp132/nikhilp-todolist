2cb2b189decde532efd7fee7f1cf23f1441f6f8a
nikhilp132 <nikhilparadkar200@gmail.com>
2021-07-01 22:32:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nikhilp-todolist
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nikhilp-todolist)
---
+++ script.js
@@ -0,0 +1,12 @@
+console.log('hello!');
+
+var listArray = [];
+function createList() {
+  var listItem = document.getElementById('item').value;
+
+  listArray.push(listItem);
+
+  var list = document.getElementById('list');
+  console.log(listArray);
+  list.innerHTML += listArray[listArray.length - 1] + '<br/>';
+}
