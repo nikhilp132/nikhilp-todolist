06ae42c0eb0acf796af98a9b610a63cd2b2e7a20
nikhilp132 <nikhilparadkar200@gmail.com>
2021-07-04 17:19:09 +0530

    2 files modified

--- script.js
+++ script.js
@@ -8,8 +8,19 @@ function createList() {
 
   var list = document.getElementById('list');
   console.log(listArray);
-  list.innerHTML += listArray[listArray.length - 1] + ' x' + '<br/>';
+  list.innerHTML +=
+    listArray[listArray.length - 1] +
+    '<span class="close">x.</span> ' +
+    '<br/>';
 }
+
+$(document).ready(function() {
+  $('.close').click(function() {
+    alert();
+  });
+});
+
+function deleteItem() {}
 
 //parent
 //parents
